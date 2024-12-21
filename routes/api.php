42fe8c1a81970d109a1f14f214b3e7042b92c634
iztileuoff <bektemir0714@gmail.com>
2024-12-21 17:20:22 +0500

    add webhook

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\WebhookController;
 use Illuminate\Support\Facades\Route;
 
 include_once __DIR__ . '/api/v1/mobile.php';
 include_once __DIR__ . '/api/v1/telegram.php';
 
+Route::post('/webhook', WebhookController::class);
